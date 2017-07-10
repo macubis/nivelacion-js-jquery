aa265a71a4ad5c0a3909e1b5c1db60ed6c182c3d
macubis <macubis@gmail.com>
2017-07-10 14:10:37 -0400

    medio de la parte 3

--- js/script-index.js
+++ js/script-index.js
@@ -10,21 +10,34 @@ $(document).ready( function(){
     texto.innerHTML = "NUEVA RECETAS";               
     $(".callout-news").append(texto);
 	}
-	printsNew();	
+	printsNew();
 
+
+/*
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 	renderHighlightedRecipes(recipesArray);
 
-});
-
+		recipesArray.forEach( function(valor, indice){
+    	console.log("En el índice " + indice + " hay este valor: " + valor);
+		});
+*/
 
 /*
 * Función que se encarga de pintar TODAS las recetas que tengan 
 * marcado el atributo "highlighted" como TRUE
 */
-function renderHighlightedRecipes(recipesArray) {
-	console.log('Recipes: ', recipesArray);
-}
+
+	function renderHighlightedRecipes(recipesArray){
+	 console.log('Recipes: ', recipesArray);
+
+	 	recipesArray.filter(function(cv){
+	 		if(cv.highlighted == true){
+	 		renderRecipe(elem);
+	 		$('.list-recipes').append('<h4>'+ elem.title + '</h4');
+	 		}
+	 	})
+
+	};
 
 /*
 * Función que se encarga de pintar UNA recetas que tenga 
@@ -32,15 +45,17 @@ function renderHighlightedRecipes(recipesArray) {
 * Aqui se tiene que crear el HTML que esta en el 
 * archivo "templates/templates-recipe.html"
 */
+
 function renderRecipe(recipe) {
+	
 	console.log('Voy a pintar la receta: ', recipe);
 }
 
 
 
-/*
+
 * Función que se encarga de pintar todas las actividades
-*/
+
 function renderActivities(activitiesArray) {
 	console.log('Activities: ', activitiesArray);
 }
@@ -49,8 +64,10 @@ function renderActivities(activitiesArray) {
 * Función que se encarga de pintar una actividad
 * Aqui se tiene que crear el HTML que esta en el 
 * archivo "templates/templates-activity.html"
-*/
-function renderActivity(recipe) {
-	
-}
 
+function renderActivity(recipe){
+
+}
+	
+*/
+});
